a760998ada3f980cc54a2f910e6e2d7e5c079272
isupov.vanya <isupov.vanya@yandex.ru>
2019-03-02 14:00:38 +0700

    typerace draft

--- extra/typerace/main.js
+++ extra/typerace/main.js
@@ -3,21 +3,45 @@
  * элемент куда будем записывать текст
  * поле ввода
  */
-const textElem;
-const inputEle;
+const textElem = document.getElementById('text_container');
+
+const inputElem = document.getElementById('checked_text');
 
 /**
  * состояние нашего приложения
  * добавить в объект текущий текст, который мы будем показывать
  */
-const state = {}
+const state = {
+  text: 'какой-то текст'
+};
 
 /**
  * отобразить текст на странице
  */
 
-
 /**
- * добавить обработкич нажания на клавиши и сравнивать введенный текст пользователем с сохраненным ранее
+ * добавить обработчик нажания на клавиши и сравнивать введенный текст пользователем с сохраненным ранее
  * если они отличаются, то изменить цвет текста на красный
  */
+
+const updateText = () => {
+  const getText = new XMLHttpRequest();
+
+  getText.onload = function() {
+    console.log(getText);
+    state.text = getText.responseText.substr(2, getText.responseText.length - 4);
+    textElem.textContent = state.text;
+  };
+  getText.open('GET', 'https://fish-text.ru/get', true);
+  getText.send();
+  return getText;
+};
+
+updateText();
+// eslint-disable-next-line func-style
+const checkText = function() {
+  this.style.color = state.text.startsWith(this.value) ? 'black' : 'red';
+};
+
+document.addEventListener('onload', updateText);
+inputElem.addEventListener('input', checkText);
